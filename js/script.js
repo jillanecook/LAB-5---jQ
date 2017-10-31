6d454e723e12d408b81e239ca593cd0b99fc62de
Jillane Cook <cookjillane@gmail.com>
2017-10-31 09:05:30 -0400

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(".table").on("click",function() {
+  	$("#popWindow").show();
+});
+
+$("#cancel").on("click", function() {    
+    $("#popWindow").hide(); 
+});
+
+
+	
+
+	
+
+
+///set up tables as objects.  
+///default all available
+//if reserve button clicked,
+	//change status = "reserved"
+
+ //if status === reserved
+ //$ change attribute of (specific object) from light gray to dark gray 
+	
