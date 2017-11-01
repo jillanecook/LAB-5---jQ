04a932313ed0cf70a395d0463d6aa30f70e7bd87
Jillane Cook <cookjillane@gmail.com>
2017-11-01 13:43:08 -0400

    basic functionality

--- js/script.js
+++ js/script.js
@@ -1,22 +1,54 @@
-$(".table").on("click",function() {
-  	$("#popWindow").show();
+$(".table.available").on("click",function openRez() {
+
+	let tableNum = $(this).text();
+	$("#popWindow").css('display', 'flex');
+	$("#tableNumber").text('Table Number:  ' + tableNum);
+	$(this).removeClass('available').addClass('selected');
+
+	$("#cancel").on("click", function() {    
+		$("#popWindow").css('display', 'none');
+		$('.selected').addClass('available');
+	}); 
+
 });
 
-$("#cancel").on("click", function() {    
-    $("#popWindow").hide(); 
-});
-
-
+$("#button").on("click", function makeRez() {
 	
+	$("#popWindow").css('display', 'none');
+	$('.selected').addClass('reserved');
+	$('.reserved').removeClass('selected');
 
-	
+	 //instead of creating individual appends, create a class for every table with
+	 //spans of name/phone/party and then call that class on mouseover. 
+
+	 $("#uName").append('.reserved');
+	 $("#phone").append('.reserved');
+	 $("#noParty").append('.reserved');
+
+	 
+
+	 let rezName = $("#uName").val();
+	 let rezPhone= $("#phone").val();
+	 let rezNoParty= $("#noParty").val();
 
 
-///set up tables as objects.  
-///default all available
-//if reserve button clicked,
-	//change status = "reserved"
+	 $(".reserved").on('mouseenter', function () {			
+	 	$(this).text(rezName);
+	 	
 
- //if status === reserved
- //$ change attribute of (specific object) from light gray to dark gray 
-	
+	 	$(".reserved").on('mouseleave', function() {
+	 		$(this).remove(rezName);
+	 	});
+
+	 	clearRezForm();
+
+	 });
+	});
+
+const clearRezForm = function() {
+	$("#uName").val("");
+	$("#phone").val("");
+	$("#noParty").val("");
+}
+
+
